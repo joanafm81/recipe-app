64ae788a61dab0847acccd06f1fa77432acdfb67
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2023-07-10 14:07:54 +0100

    Added login and logout features

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView #to display lists and details
 from .models import Recipe #to access Recipe model
+from django.contrib.auth.mixins import LoginRequiredMixin #to protect class-based view
 
 # Create your views here.
 # def recipes_home(request):
@@ -10,10 +11,10 @@ class HomeRecipeListView(ListView):             #class-based view
    model = Recipe                               #specify model
    template_name = 'recipes/recipes_home.html'  #specify template 
 
-class RecipeListView(ListView):           #class-based view
+class RecipeListView(LoginRequiredMixin, ListView):           #class-based “protected” view
    model = Recipe                         #specify model
    template_name = 'recipes/main.html'    #specify template 
 
-class RecipeDetailView(DetailView):                       #class-based view
+class RecipeDetailView(LoginRequiredMixin, DetailView):                       #class-based “protected” view
    model = Recipe                                       #specify model
    template_name = 'recipes/detail.html'                 #specify template
